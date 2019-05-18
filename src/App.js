84dd954a937782fe22e9a241d55366bcb9deaac0
ZemaToxic <ZemaToxic@users.noreply.github.com>
2019-05-18 15:58:39 +1200

    Small changes, starting modularity

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import './app.css'
+import Home from './pages/Home'
+import Info from './pages/Info'
+import Login from './pages/Login'
 
-
-function Index() {
-    return <h2>Home</h2>;
-  }
-  
   function Bots() {
       return <h2>Bots</h2>
   }
@@ -18,22 +15,13 @@ function Index() {
     return <h2>Twitch Bots</h2>
 }
 
-  function Info() {
-    return <h2>Info</h2>;
-  }
-  
-  function Login() {
-    return <h2>Login</h2>;
-  }
-  
-
 class App extends Component {
     render() {
         return (
             <div className="App" >
+                <Router>
                 <div className="container">
                     <div className="sidebar">
-                        <Router>
                             <ul>
                                 <li>
                                     <Link to="/"><h2>Home</h2></Link>
@@ -54,21 +42,18 @@ class App extends Component {
                                     <Link to="/login"><h2>Login</h2></Link>
                                 </li>
                             </ul>
-
-                            <Route path="/" exact component={Index} />
+                    </div>
+                    <div className="content">
+                        <h1>ZemaToxic Bots</h1>
+                            <Route path="/" exact component={Home} />
                             <Route path="/bots/" component={Bots} />
                             <Route path="/discordbots/" component={DiscordBots} />
                             <Route path="/twitchbots/" component={TwitchBots} />
                             <Route path="/info/" component={Info} />
                             <Route path="/login/" component={Login} />
-                        </Router>
-                    </div>
-                    <div className="content">
-                        <h1>ZemaToxic Bots</h1>
-                        Test home page goes here.
-
                     </div>
                 </div>
+                </Router>
             </div>
         );
     }
