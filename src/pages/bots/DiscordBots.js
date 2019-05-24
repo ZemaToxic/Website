22cd417dcdb001549e180b9a6326d7078e09a597
ZemaToxic <ZemaToxic@users.noreply.github.com>
2019-05-25 11:10:28 +1200

    Redo API link

--- src/pages/bots/DiscordBots.js
+++ src/pages/bots/DiscordBots.js
@@ -14,7 +14,7 @@ const INITIAL_STATE = {
     fetchData () {
       this.setState({ ...INITIAL_STATE, isFetching: true })
       if(!INITIAL_STATE.isFetching){
-        fetch('https://api.zematoxic.com/discord/commands')
+        fetch('https://api.zematoxic.com/bots/commands')
         .then((data) => data.json())
         .then((data) => this.setState({ ...INITIAL_STATE, data }))
         .catch((error) => this.setState({ ...INITIAL_STATE, error }))
