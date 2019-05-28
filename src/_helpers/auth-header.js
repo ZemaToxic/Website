b46e315ac122f1e5686471bb0d184cb1727495c9
ZemaToxic <ZemaToxic@users.noreply.github.com>
2019-05-29 11:57:21 +1200

    Loggin authheader

--- src/_helpers/auth-header.js
+++ src/_helpers/auth-header.js
@@ -1,6 +1,8 @@
 export function authHeader() {
     // return authorization header with jwt token
     let user = JSON.parse(localStorage.getItem('user'));
+    
+    console.log(user)
 
     if (user && user.token) {
         return { 'Authorization': 'Bearer ' + user.token };
